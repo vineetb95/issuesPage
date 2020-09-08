f7682f40796c95c73078ee0b5a54c03cb125e886
Vineet Bakshi <vineet.bakshi95@gmail.com>
2020-09-08 16:29:06 +0530

    /list-issues api
    Signed-off-by: Vineet Bakshi <vineet.bakshi95@gmail.com>

--- issues-page/src/controllers/issue.controller.ts
+++ issues-page/src/controllers/issue.controller.ts
@@ -22,9 +22,8 @@ import {IssueRepository} from '../repositories';
 export class IssueController {
   constructor(
     @repository(IssueRepository)
-    public issueRepository: IssueRepository,
-  ) {
-  }
+    public issueRepository : IssueRepository,
+  ) {}
 
   @post('/add-issue', {
     responses: {
@@ -40,13 +39,60 @@ export class IssueController {
         'application/json': {
           schema: getModelSchemaRef(Issue, {
             title: 'NewIssue',
-            exclude: ['id']
+            exclude:['id']
           }),
         },
       },
     })
-      issue: Issue,
+    issue: Issue,
   ): Promise<Issue> {
     return this.issueRepository.create(issue);
   }
+
+  @get('/list-issues', {
+    responses: {
+      '200': {
+        description: 'Array of Issue model instances',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: getModelSchemaRef(Issue, {includeRelations: true}),
+            },
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.number('page') pageNo=1,
+  ): Promise<Issue[]> {
+    return this.issueRepository.find({
+      offset: ((pageNo-1)*10),
+      limit: 10,
+    });
+  }
+
+  @patch('/update-issue/{id}', {
+    responses: {
+      '204': {
+        description: 'Issue PATCH success',
+      },
+    },
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Issue, {partial: true}),
+        },
+      },
+    })
+    issue: Omit<Issue,'id'>,
+  ): Promise<void> {
+    await this.issueRepository.updateById(id, issue);
+  }
+
+
 }
